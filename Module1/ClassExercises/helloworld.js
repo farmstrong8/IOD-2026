dcc6dddca4e6ebe8fbe5c742d77c5d698105775f
Ferris Armstrong <fda@bu.edu>
2026-06-17 16:44:46 -0400

    javascript

---
+++ Module1/ClassExercises/helloworld.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+function sayHello() {
+    console.log("Hello");
+}
+
+sayHello();
+
+function sum(a, b) {
+    // console.log(a + b);
+    return a + b;
+}
+
+sum(1, 2);
+console.log(sum(1, 2));
